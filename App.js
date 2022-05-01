6160f737788abbd7cfeff110ffe4b249adc0f3f3
DhanashreeSB <123dhanashri@gmail.com>
2022-05-02 01:02:35 +0530

    Nested navigators added

--- App.js
+++ App.js
@@ -3,10 +3,58 @@ import { StyleSheet, Button, View } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 import { MealsOverviewScreen } from "./screens/MealsOverviewScreen";
 import { MealDetailScreen } from "./screens/MealDetailScreen";
+import { FavoritesScreen } from "./screens/FavoritesScreen";
+import "react-native-gesture-handler";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#351401",
+        },
+        headerTintColor: "white",
+        //Background color can be set using contentColor property
+        sceneContainerStyle: {
+          backgroundColor: "#3f2f25",
+        },
+        drawerContentStyle: {
+          backgroundColor: "#351401",
+        },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -26,10 +74,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: "All categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen
@@ -43,14 +92,19 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{
-            // We can add button/element in Header section using following option.
-            // But if we want the onPress of button in header to interact with the screen components, then it won't be possible in App.js.
-            // So to deal with the issue mentioned above, we have another alternative. We can set this option in the screen dirctly.
-            // headerRight: () => {
-            //   return <Button title={'Tap me!'}/>
-            // }
-          }}/>
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailScreen}
+            options={{
+              title: "About the meal",
+              // We can add button/element in Header section using following option.
+              // But if we want the onPress of button in header to interact with the screen components, then it won't be possible in App.js.
+              // So to deal with the issue mentioned above, we have another alternative. We can set this option in the screen dirctly.
+              // headerRight: () => {
+              //   return <Button title={'Tap me!'}/>
+              // }
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
